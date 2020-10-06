4fd8821e47a2a52c79943930a886960fb5f0258e
Caleb <CalebMc@users.noreply.github.com>
2020-10-06 08:23:50 -0700

    Orginized the files.
    UNCHANGED: "game.js", and "index.html"
    
    NEW: "game2.js" and "index2.html"
    
    You can push your changed "game.js" now that you have made some changed on your own.
    
    My "game2.js" should be just about the same as yours it just is arranged differently.

--- game2.js
+++ game2.js
@@ -1,53 +1,61 @@
+let playerScore = 0;
+let compScore = 0;
+let drawScore = 0;
+let playerChoice = 0;
+let computerChoice = 0;
+    
 game();
 
-function game(){
 
-    let playerScore = 0;
-    let compScore = 0;
-    let drawScore = 0;
-    let playerChoice = 0;
-    let computerChoice = 0;
-    
-    let i = 0; 
-    for (i; i < 5; i++) { 
+/* The main function that calls on other functions to play the game
+ */
+function game(){ 
+    for (i= 0; i < 5; i++) { 
         playerChoice = playerSelection();
         computerChoice = computerPlay();
-        
         playRound(playerChoice, computerChoice);
-        
-        function playerSelection() {
-            let playerChoice = prompt("Rock paper scissors?");
-            return playerChoice.toLowerCase();
-        }
-        
-
-        function computerPlay () {
-            let select = ["rock", "paper", "scissors"];
-            let computerChoice = select[Math.floor(Math.random()*select.length)];
-            return computerChoice;
-        }
-        
-        function playRound(playerChoice, computerChoice) {
-
-            if (playerChoice == computerChoice) {
-                console.log("Draw!")
-                drawScore++;
-            } else if (playerChoice == "rock" && computerChoice == "scissors"){
-                console.log("You Win!")
-                playerScore++;
-            } else if (playerChoice == "paper" && computerChoice == "rock"){
-                console.log("You Win!")
-                playerScore++;
-            } else if (playerChoice == "scissors" && computerChoice == "paper"){
-                console.log("You Win!")
-                playerScore++;
-            } else {
-                console.log("You Lose!")
-                compScore++;
-            }
-        }
     }
         console.log("Wins: " + playerScore);
         console.log("Loses: " + compScore);
         console.log("Ties: " + drawScore);
+}
+
+/* Function that gets the user's input via prompt. The coverts it to lowercase.
+ * @Returns playerChoice
+ */
+function playerSelection() {
+    let playerChoice = prompt("Rock paper scissors?");
+    return playerChoice.toLowerCase();
+}
+    
+/* Function that chooses a random element in a list of strings for the computer's choice.
+ * @Returns computerChoice
+ */
+function computerPlay () {
+    let select = ["rock", "paper", "scissors"];
+    let computerChoice = select[Math.floor(Math.random()*select.length)];
+    return computerChoice;
+}
+
+/* The logic of the game. Compares the player's choice with the computer's choice
+ * Prints out the results of total win,s loses, and ties.
+ * Takes two Strings a paramaters. 
+ */
+function playRound(playerChoice, computerChoice) {
+    if (playerChoice == computerChoice) {
+        console.log("Draw!")
+        drawScore++;
+    } else if (playerChoice == "rock" && computerChoice == "scissors"){
+        console.log("You Win!")
+        playerScore++;
+    } else if (playerChoice == "paper" && computerChoice == "rock"){
+        console.log("You Win!")
+        playerScore++;
+    } else if (playerChoice == "scissors" && computerChoice == "paper"){
+        console.log("You Win!")
+        playerScore++;
+    } else {
+        console.log("You Lose!")
+        compScore++;
+    }
 }
